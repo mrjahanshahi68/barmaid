d0ee60d3170934aa2281878e4413836ed8b97af2
mrjahanshahi <mr.jahanshahi@gmail.com>
2019-09-27 17:37:43 +0330

    route configed

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,12 +1,23 @@
 import * as React from "react";
+import { Route ,BrowserRouter as Router  } from "react-router-dom";
+import  NavbarMenu  from './NavbarMenu'
+import { Dashboard } from "./Dashboard";
+import { FoodCategory } from "./FoodCategory";
 
-export default class App extends React.Component<{},{}>{
+export default class App extends React.Component<{}, {}>{
 
     render() {
         return (
-           <div className="container">
-               Home
-           </div>
+            <div>
+                <Router>
+                    <NavbarMenu />
+
+                    <div className="main-content">
+                        <Route exact path="/" component={Dashboard}></Route>
+                        <Route exact path="/food-category" component={FoodCategory}></Route>
+                    </div>
+                </Router>
+            </div >
 
         );
     }
---
+++ src/Components/Dashboard.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+export class Dashboard extends React.Component<{},{}>{
+    render(){
+        return <h1>dashboard</h1>
+    }
+}
